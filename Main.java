62c238224b01a1999584c49557236989c4974e06
unknown <josu49067@gmail.com>
2022-08-07 23:44:40 -0600

    calculadora

---
+++ Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author alumno
+ */
+
+public class Main {
+    
+    public static void main(){
+        
+        frm_calculadora frm = new frm_calculadora();
+       frm.show(); 
+       
+      
+       
+    }
+            
+    
+}
